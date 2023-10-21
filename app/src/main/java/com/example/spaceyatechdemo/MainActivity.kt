5e231f2122d3b698c0f6c7f8debabcb9da4ad951
Okello Sam <samuelokello929@gmail.com>
2023-10-21 21:25:28 +0300

    initial commit

--- app/src/main/java/com/example/spaceyatechdemo/MainActivity.kt
+++ app/src/main/java/com/example/spaceyatechdemo/MainActivity.kt
@@ -3,15 +3,21 @@ package com.example.spaceyatechdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -23,12 +29,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             SpaceYaTechDemoTheme {
                 // A surface container using the 'background' color from the theme
-                Column {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                ) {
                     Text(
                         text = stringResource(id = R.string.upcoming),
+                        style = MaterialTheme.typography.titleLarge,
                     )
+                    Spacer(modifier = Modifier.height(25.dp))
                     Row {
                         MyCard(name = stringResource(id = R.string.dates_dinner))
+                        Spacer(modifier = Modifier.width(16.dp))
                         MyCard(name = stringResource(id = R.string.beach_sand))
                     }
                 }
@@ -44,9 +57,13 @@ fun MyCard(
     Card(
         colors = CardDefaults.cardColors(Color.Black),
         modifier = Modifier
-            .size(width = 40.dp, height = 100.dp)
+            .size(width = 100.dp, height = 100.dp),
 
     ) {
-        Text(text = name, color = Color.White)
+        Text(
+            text = name,
+            color = Color.White,
+            style = MaterialTheme.typography.bodyMedium,
+        )
     }
 }
