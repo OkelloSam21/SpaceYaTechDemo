825cc21300c790b6cb6484c920b161997c83ec86
Okello Sam <samuelokello929@gmail.com>
2023-12-02 10:08:56 +0300

    fix test for card components

--- app/src/androidTest/java/com/ComposeUiTest.kt
+++ app/src/androidTest/java/com/ComposeUiTest.kt
@@ -1,7 +1,9 @@
 package com
 
+import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithText
 import com.example.spaceyatechdemo.DisplayContent
 import com.example.spaceyatechdemo.MyCard
@@ -14,16 +16,16 @@ class ComposeUiTest {
     val composeTestRule = createComposeRule()
 
     @Test
-    fun DisplayContentTest() {
+    fun displayContentTest() {
         composeTestRule.setContent {
             DisplayContent()
         }
 
-        // check if upcoming trips is displaed on the screen
+        // check if upcoming trips is displayed on the screen
         composeTestRule.onNodeWithText("Upcoming Trips").assertIsDisplayed()
 
-        // check if the card component is displayed on the screen
-        composeTestRule.onNodeWithText("my_card").assertIsDisplayed()
+        // check if the 2 card component are displayed on the screen
+        composeTestRule.onAllNodesWithTag("my_card").assertCountEquals(2)
     }
 
     @Test
